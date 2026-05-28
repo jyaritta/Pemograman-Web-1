5a6a99f133a2d170139b603b235a4e44e8f5303b
jyaritta <rahmasyaritazaya@gmail.com>
2026-05-29 03:05:27 +0800

    modul 5

---
+++ Modul5/file.py
@@ -0,0 +1,17 @@
+filenames = [
+    'PRAK501_RahmaSyaritaJaya_2510817320004.html',
+    'PRAK502_RahmaSyaritaJaya_2510817320004.html',
+    'PRAK503_RahmaSyaritaJaya_2510817320004.html',
+    'PRAK504_RahmaSyaritaJaya_2510817320004.html',
+    'PRAK505_RahmaSyaritaJaya_2510817320004.html',
+    'PRAK506_RahmaSyaritaJaya_2510817320004.html',
+    'PRAK507_RahmaSyaritaJaya_2510817320004.html',
+    'PRAK508_RahmaSyaritaJaya_2510817320004.html',
+    'PRAK509_RahmaSyaritaJaya_2510817320004.html',
+    'PRAK5010_RahmaSyaritaJaya_2510817320004.html',
+    'PRAK50811RahmaSyaritaJaya_2510817320004.html',
+]
+
+for file in filenames:
+    with open(file, 'w') as f:
+        pass
