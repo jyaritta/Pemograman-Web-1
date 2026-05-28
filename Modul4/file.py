0b235e0049eb0e05c46fe3d4384570b932f52df5
jyaritta <rahmasyaritazaya@gmail.com>
2026-05-29 02:47:29 +0800

    update modul 4

---
+++ Modul4/file.py
@@ -0,0 +1,14 @@
+filenames = [
+    'PRAK401_RahmaSyaritaJaya_2510817320004.html',
+    'PRAK402_RahmaSyaritaJaya_2510817320004.html',
+    'PRAK403_RahmaSyaritaJaya_2510817320004.html',
+    'PRAK404_RahmaSyaritaJaya_2510817320004.html',
+    'PRAK405_RahmaSyaritaJaya_2510817320004.html',
+    'PRAK406_RahmaSyaritaJaya_2510817320004.html',
+    'PRAK407_RahmaSyaritaJaya_2510817320004.html',
+    'PRAK408_RahmaSyaritaJaya_2510817320004.html',
+]
+
+for file in filenames:
+    with open(file, 'w') as f:
+        pass
